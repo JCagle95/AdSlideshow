de5fee55841d7c2f16e7befd2636255df19977d0
Jackson Cagle <jackson.cagle@gmail.com>
2018-08-31 20:57:27 -0400

    Added Video

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {Content: [{url: "images/img1.jpg", type: "image"},
+                                 {url: "images/img2.jpg", type: "image"},
+                                 {url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", type: "video"},
+                                 {url: "images/img3.jpg", type: "image"}],
+                       Delay: [5,
+                               5,
+                               15,
+                               5] });
 });
 
 module.exports = router;
